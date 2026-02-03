7559a6f92ecfeb2b68daadb07b6169030c4f01f2
LollibagTrash <rutai.anika2006@gmail.com>
2026-02-03 10:42:58 +0100

    első tíz

---
+++ Gyakorlatok/AverageOfArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AverageOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = { 10, 20, 30, 40 };
+            //double avg = numbers.Average();
+
+            int[] numbers;
+            int tombHossz;
+            string inputText;
+            double avg;
+
+            Console.WriteLine("Adjunk be egész számokat");
+            Console.WriteLine("Hány számot olvassunk be?");
+            inputText = Console.ReadLine();
+            tombHossz = Convert.ToInt32(inputText);
+
+            numbers = new int[tombHossz];
+
+            for (int i = 0; i < tombHossz; i++)
+            {
+                Console.WriteLine($"Kérjük a(z) {i+1}. számot:");
+                inputText = Console.ReadLine();
+                numbers[i] = Convert.ToInt32(inputText);
+            }
+
+            avg = numbers.Average();
+            Console.WriteLine($"Average: {avg}");
+            Console.WriteLine();
+            Console.WriteLine("Enterre kilépés!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Gyakorlatok/TeruletKerulet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TeruletKerulet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, c, K, T, S;
+            string inputText;
+            Console.WriteLine("Háromszög Kerület/Terület számítás");
+
+            Console.WriteLine("Add meg a háromszög első oldalhosszát");
+            inputText = Console.ReadLine();
+            double.TryParse(inputText, out a);
+
+            Console.WriteLine("Add meg a háromszög második oldalhosszát");
+            inputText = Console.ReadLine();
+            double.TryParse(inputText, out b);
+
+            Console.WriteLine("Add meg a háromszög harmadik oldalhosszát");
+            inputText = Console.ReadLine();
+            double.TryParse(inputText, out c);
+
+            K = a + b + c;
+
+            S = K / 2;
+
+            T = Math.Sqrt(S * (S - a) * (S - b) * (S - c)); 
+
+            Console.WriteLine($"Kerület: {K}");
+            Console.WriteLine($"Terület: {T}");
+            Console.WriteLine();
+            Console.WriteLine("Enterre kilépés!");
+            Console.ReadLine();
+
+        }
+
+    }
+}
