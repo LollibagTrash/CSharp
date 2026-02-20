27d0f007adbdb379c7991b855881f701ceede28d
LollibagTrash <rutai.anika2006@gmail.com>
2026-02-20 08:48:19 +0100

    Add files via upload
    kijavított első tíz

---
+++ Gyakorlatok/Palindrome2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Palindrome2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputText = " ";
+            string reverseText = " ";
+            char[] reverse_array = null;
+
+
+
+            Console.WriteLine("Palindrome szókereső");
+            Console.WriteLine();
+            Console.Write("Kérek egy szót: ");
+            inputText = Console.ReadLine();
+
+            reverse_array = inputText.ToCharArray();
+            Array.Reverse(reverse_array);
+            reverseText = new string(reverse_array);
+
+            if (inputText.Equals(reverseText))
+            {
+                Console.WriteLine("A beírt szó palindrom!");
+            }
+            else
+            {
+                Console.WriteLine("A beírt szó nem palindrom!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enterre kilépés!");
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Gyakorlatok/ParosParatlan/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ParosParatlan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy egész számot:");
+            string inputText = Console.ReadLine();
+            int n = 0;
+            int.TryParse(inputText, out n);
+            if (n % 2 == 0)
+            {
+                Console.Write(n + " páros szám!");
+            }
+            else
+            {
+                Console.Write(n + " páratlan szám!");
+                Console.WriteLine();
+                Console.WriteLine("Enterre kilépés!");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
